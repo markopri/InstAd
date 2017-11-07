b048937d0fb21241fd2805790f7c21c575b08d85
kile3r <gasljevic.kristijan@gmail.com>
2017-11-07 23:31:48 +0100

    Added login function and alert if login is not successful

--- InstaAd/InstaAd/UI/Login/IALoginViewController.swift
+++ InstaAd/InstaAd/UI/Login/IALoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class IALoginViewController: UIViewController {
     
@@ -20,7 +21,32 @@ class IALoginViewController: UIViewController {
             
         }else{
             
-            //TODO login
+            Auth.auth().signIn(withEmail: txtUsername.text!, password: txtPassword.text!) { (user, error) in
+                
+                if error == nil {
+                    //TODO - izbriši nakon redirekcije na stranicu početnu
+                    let alertController = UIAlertController(title: "Login good", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                    //TODO - otkomentiraj kad ce bit prebacivanje na glavnu stranicu
+                    //Go to the HomeViewController if the login is sucessful
+                    //let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                    //self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    
+                    //Tells the user that there is an error and then gets firebase to tell them the error
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
             
             
         }
