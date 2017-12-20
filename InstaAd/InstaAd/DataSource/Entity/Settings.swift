b4af60c43dcbd1e1395e2da829625512cc9996d3
Marko Koprivnjak <koprivnjak.marko@hotmail.com>
2017-12-21 00:09:34 +0100

    Partialy changed Settings tab

---
+++ InstaAd/InstaAd/DataSource/Entity/Settings.swift
@@ -0,0 +1,22 @@
+//
+//  Settings.swift
+//  InstaAd
+//
+//  Created by Marko Koprivnjak on 18/12/2017.
+//
+
+import UIKit
+
+class Settings: NSObject {
+
+    var userEmail : String?;
+    var userRegistrationDate: String?;
+    var userName: String?;
+
+    var userCurrentPassword : String?;
+    var userNewPassword : String?;
+    var userRepeatNewPassword : String?;
+
+    var action : String?;
+
+}
--- InstaAd/InstaAd/UI/Settings/IASettingsViewController.swift
+++ InstaAd/InstaAd/UI/Settings/IASettingsViewController.swift
@@ -53,9 +53,9 @@ class IASettingsViewController: UIViewController, UITableViewDelegate, UITableVi
         switch section
         {
         case 0:
-            return 2;
+            return 4;
         case 1:
-            return 3;
+            return 4;
         default:
             return 1;
         }
@@ -64,42 +64,74 @@ class IASettingsViewController: UIViewController, UITableViewDelegate, UITableVi
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if (indexPath.section == 0)
         {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "IASettingsUserDataTableViewCell", for: indexPath) as! IASettingsUserDataTableViewCell;
             if (indexPath.row == 0)
             {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "IASettingsUserDataTableViewCell", for: indexPath) as! IASettingsUserDataTableViewCell;
                 cell.lblUserDataLabel.text = "Email";
+                editSettingsUserDataTableViewCell(cell: cell);
                 cell.txtUserDataValue.text = "user email from firebase";
                 cell.txtUserDataValue.isEnabled = false;
+                return cell;
             }
-            else
+            else if (indexPath.row == 1)
             {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "IASettingsUserDataTableViewCell", for: indexPath) as! IASettingsUserDataTableViewCell;
                 cell.lblUserDataLabel.text = "Registration date";
+                editSettingsUserDataTableViewCell(cell: cell);
                 cell.txtUserDataValue.text = "Date of registration from firebase";
                 cell.txtUserDataValue.isEnabled = false;
+                return cell;
             }
-            return cell;
+            else if (indexPath.row == 2)
+            {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "IASettingsUserDataTableViewCell", for: indexPath) as! IASettingsUserDataTableViewCell;
+                cell.lblUserDataLabel.text = "Username";
+                editSettingsUserDataTableViewCell(cell: cell);
+                return cell;
+            }
+            else{
+                let aCell = tableView.dequeueReusableCell(withIdentifier: "IASettingsButtonTableViewCell", for: indexPath) as! IASettingsButtonTableViewCell;
+                aCell.btnSettingsButton.setTitle("Save username", for: UIControlState.normal);
+                aCell.btnSettingsButton.backgroundColor = UIColor .green;
+                editSettingsButtonTableViewCell(cell: aCell);
+
+                aCell.btnSettingsButton.tag = indexPath.section;
+                aCell.btnSettingsButton.addTarget(self,action:#selector(settingsButtonClicked(sender:)), for: .touchUpInside);
+                return aCell;
+            }
         }
         else if (indexPath.section == 1)
         {
             if (indexPath.row == 0)
             {
                 let cell = tableView.dequeueReusableCell(withIdentifier: "IASettingsUserDataTableViewCell", for: indexPath) as! IASettingsUserDataTableViewCell;
-                cell.lblUserDataLabel.text = "New password";
-                cell.txtUserDataValue.text = "";
+                cell.lblUserDataLabel.text = "Current password";
+                editSettingsUserDataTableViewCell(cell: cell);
                 return cell;
             }
             else if (indexPath.row == 1)
             {
                 let cell = tableView.dequeueReusableCell(withIdentifier: "IASettingsUserDataTableViewCell", for: indexPath) as! IASettingsUserDataTableViewCell;
+                cell.lblUserDataLabel.text = "New password";
+                editSettingsUserDataTableViewCell(cell: cell);
+                return cell;
+            }
+            else if (indexPath.row == 2)
+            {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "IASettingsUserDataTableViewCell", for: indexPath) as! IASettingsUserDataTableViewCell;
                 cell.lblUserDataLabel.text = "Repeat password";
-                cell.txtUserDataValue.text = "";
+                editSettingsUserDataTableViewCell(cell: cell);
                 return cell;
             }
             else
             {
                 let aCell = tableView.dequeueReusableCell(withIdentifier: "IASettingsButtonTableViewCell", for: indexPath) as! IASettingsButtonTableViewCell;
                 aCell.btnSettingsButton.setTitle("Save password", for: UIControlState.normal);
-                aCell.btnSettingsButton.backgroundColor = UIColor .blue;
+                aCell.btnSettingsButton.backgroundColor = UIColor .green;
+                editSettingsButtonTableViewCell(cell: aCell);
+
+                aCell.btnSettingsButton.tag = indexPath.section;
+                aCell.btnSettingsButton.addTarget(self,action:#selector(settingsButtonClicked(sender:)), for: .touchUpInside);
                 return aCell;
             }
         }
@@ -108,7 +140,57 @@ class IASettingsViewController: UIViewController, UITableViewDelegate, UITableVi
             let aCell = tableView.dequeueReusableCell(withIdentifier: "IASettingsButtonTableViewCell", for: indexPath) as! IASettingsButtonTableViewCell;
             aCell.btnSettingsButton.setTitle("Logout", for: UIControlState.normal);
             aCell.btnSettingsButton.backgroundColor = UIColor .red;
+            editSettingsButtonTableViewCell(cell: aCell);
+
+            aCell.btnSettingsButton.tag = indexPath.section;
+            aCell.btnSettingsButton.addTarget(self,action:#selector(settingsButtonClicked(sender:)), for: .touchUpInside);
             return aCell;
         }
+    }
+
+    func editSettingsUserDataTableViewCell(cell: IASettingsUserDataTableViewCell) -> Void {
+        cell.txtUserDataValue.text = "";
+        cell.txtUserDataValue.layer.borderWidth = 1;
+        cell.txtUserDataValue.layer.cornerRadius = 5;
+        cell.txtUserDataValue.layer.borderColor = UIColor.black.cgColor;
+    }
+
+    func editSettingsButtonTableViewCell(cell: IASettingsButtonTableViewCell) -> Void {
+        let shiftX = (cell.frame.width - cell.btnSettingsButton.frame.width) / 2;
+        let shiftY = (cell.btnSettingsButton.frame.height - 40) / 2;
+        cell.btnSettingsButton.frame = CGRect (x: shiftX, y: shiftY, width: cell.btnSettingsButton.frame.width, height: 40);
+        cell.btnSettingsButton.layer.cornerRadius = 5;
+        cell.btnSettingsButton.layer.borderWidth = 2;
+    }
+
+    @objc func settingsButtonClicked(sender:UIButton){
+        let buttonSection  = sender.tag;
+        NSLog("Odabrani gumb: ");
+        NSLog(String(buttonSection));
+        switch buttonSection {
+        case 0:
+            changeUserName();
+            break;
+        case 1:
+            changeUserPassword();
+            break;
+        case 2:
+            logout();
+            break;
+        default:
+            NSLog("Unknown selection");
+        }
+    }
+
+    func changeUserName() -> Void {
+        NSLog("Method for changing user name in database");
+    }
+
+    func changeUserPassword() -> Void {
+        NSLog("Method for changing user password in database");
+    }
+
+    func logout() -> Void {
+        NSLog("Method for logout");
     }
 }
